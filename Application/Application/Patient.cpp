57df4a2e17b90d9e58e0ddecb03dbff7c1d7d80a
Allan Christopher Blundell <allforme3@msn.com>
2016-05-05 16:31:27 +0100

    Patient Update
    Updated Header and Class

--- Application/Application/Patient.cpp
+++ Application/Application/Patient.cpp
@@ -10,3 +10,24 @@ Patient::Patient()
 Patient::~Patient()
 {
 }
+
+void Patient::returnPatientName(char* buffer)
+{
+
+}
+
+void Patient::returnPatientAddress(char* buffer)
+{
+
+}
+
+bool Patient::changePatientAddress(char * addressone, char * addresstwo, char* addressthree)
+{
+	return false;
+}
+
+bool Patient::changePatientName(char *)
+{
+	return false;
+}
+
--- Application/Application/Patient.h
+++ Application/Application/Patient.h
@@ -1,8 +1,46 @@
 #pragma once
+#define MAX_SIZE 258
+#include "Treatment.h"
 class Patient
 {
+	
+private:
+	int PatientID;
+	char PatientName[MAX_SIZE];
+	char Address1stLine[MAX_SIZE];
+	char Address2ndLine[MAX_SIZE];
+	char Address3rdLine[MAX_SIZE];
+	char City[MAX_SIZE];
+	char Postcode[MAX_SIZE];
+	char Email[MAX_SIZE];
+	char TelNo[MAX_SIZE];
+	char DOB[11];
+	char NHSNumber[MAX_SIZE];
+	bool Gender;
+	char PatientDoctor[MAX_SIZE];
+	char SurgeryName[MAX_SIZE];
+	char StoredConsentForm[4048];
+	char MedicalHistory[MAX_SIZE][4048];
+	char SurgicalHistory[MAX_SIZE][4048];
+	char FamilyHistory[MAX_SIZE][4048];
+	char SocialHistory[MAX_SIZE][4048];
+	char MedicationGiven[MAX_SIZE][4048];
+	char Allergies[MAX_SIZE][4048];
+	char SignificatanceMHistory[MAX_SIZE];
+	char InitialTreatStrategy[MAX_SIZE];
+	Treatment** PreviousTreatments;
+	char BillingInfo[MAX_SIZE];
 public:
 	Patient();
 	~Patient();
+private:
+	void returnPatientName(char*);
+	void returnPatientAddress(char*);
+	bool changePatientAddress(char*, char*, char*);
+	bool changePatientName(char*);
+	void returnIntTreatmentStrat(char*);
+	bool saveConsentForm(char*);
+	void returnNHSNumber(char*);
+	void returnPatientEmail(char*);
 };
 
